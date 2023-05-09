d9c3c871b51771709bff40a4fdeb68dc697b38e8
Crypt0nic0 <n.greneche45@gmail.com>
2023-05-09 21:58:19 +0200

    TP Finished

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-// Créer la logique de compte à rebours
+let totalSeconds = 0;
+let minutes = 0;
+let seconds = 0;
 
-choice.value = 5 minutes
 
-choice.value * 60 = totalSeconds
-
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60
-
-// Créer un événement à la validation du form pour lancer le compte à rebours
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    totalSeconds = choice.value * 60;
+    
+    let cpt = setInterval(() => {
+        minutes = Math.floor(totalSeconds / 60);
+        seconds = totalSeconds % 60;
+        countdownDisplay.innerHTML = `<p>${minutes}:${seconds}</p>`;
+        totalSeconds -= 1;
+    }, 1000);
+    
+    setTimeout(() => {
+        clearInterval(cpt);
+    }, totalSeconds * 1000);
+});
